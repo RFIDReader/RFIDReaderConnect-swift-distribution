1d2c4e8053aeeaed67b583386f48ee79602d4de0
Pratham Jaiswal <pr47h4m@gmail.com>
2026-06-27 16:57:37 +0530

    cleaned older distributions

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RFIDReaderConnect",
     platforms: [
-        .macOS(.v13), .iOS(.v13)
+        .macOS(.v13), .iOS(.v13),
     ],
     products: [
         .library(
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RFIDReaderConnect",
-            url: "https://github.com/RFIDReader/RFIDReaderConnect-swift-distribution/raw/refs/heads/master/releases/download/v0.0.1-test/RFIDReaderConnect.xcframework.zip",
-            checksum: "6603f98cd2e1f7b989ea67ac4bf10e33a9d6dfb1bcf8e8c6e1abb8db06b20a4e"
+            url: "",
+            checksum: ""
         )
     ]
 )
